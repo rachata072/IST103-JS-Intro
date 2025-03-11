6eb7c3e653233682645306b190ddc33df7b12276
SputnikTH <rachata9866@gmail.com>
2025-03-10 20:23:39 -0700

    Rachata-Class Activity 2-10 March 2025 #2

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 window.alert("Hello World");
 
-const StringVar = "Trey";
+const StringVar = "Rachata";
 let IntVar = 27;
 
-console.log("Person name is " + StringVar + "\nMy Age is " + IntVar);
+const myInfo = + StringVar + "\nMy Age is " + IntVar;
+
+console.log(myInfo);
 
