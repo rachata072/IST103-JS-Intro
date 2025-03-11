328403078f2a0d90a17e0c6755e7f8c247fc8c99
SputnikTH <rachata9866@gmail.com>
2025-03-10 20:26:13 -0700

    Rachata-Class Activity 2-10 March 2025 #3

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.alert("Hello World");
 const StringVar = "Rachata";
 let IntVar = 27;
 
-const myInfo = + StringVar + "\nMy Age is " + IntVar;
+const myInfo = StringVar;
 
-console.log(myInfo);
+console.log("My name is: ", myInfo);
 
